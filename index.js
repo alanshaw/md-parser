ad1d05c7fa22126b742fa2650889d9093a3822ef
Alan Shaw <alan@freestyle-developments.co.uk>
2014-09-18 09:51:55 +0100

    Fix paragraphs

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ function Env (parent) {
   this.chunks = []
 }
 
+function inList (env) {
+  return env.closeTag == "</li>" || (env.parent && (env.parent.closeTag == "</ul>" || env.parent.closeTag == "</ol>"))
+}
+
+function inEmphasis (env) {
+  return env.closeTag == "</em>" || env.closeTag == "</strong>"
+}
+
 function parse (chunk, env, tr) {
   switch (chunk.type) {
     case "heading":
@@ -20,21 +28,19 @@ function parse (chunk, env, tr) {
       break
     case "star":
       if (!env.closeTag) {
-        if (!env.inList) {
+        if (inList(env)) {
           tr.push("<ul>")
           env = new Env(env)
-          env.inList = true
           env.closeTag = "</ul>"
         }
         tr.push("<li>")
         env = new Env(env)
         env.closeTag = "</li>"
       } else {
-        if (!env.inEmphasis) {
+        if (!inEmphasis(env)) {
           env = new Env(env)
           tr.push("<em>")
           env.inline = true
-          env.inEmphasis = true
           env.closeTag = "</em>"
         } else {
           tr.push(env.closeTag)
@@ -43,15 +49,18 @@ function parse (chunk, env, tr) {
       }
       break
     case "emphasis":
-      if (!env.inEmphasis) {
+      if (!env.closeTag) {
+        tr.push("<p>")
+        env.closeTag = "</p>"
+      }
+
+      if (!inEmphasis(env)) {
         env = new Env(env)
         if (chunk.content == "__" || chunk.content == "**") {
           tr.push("<strong>")
-          env.inEmphasis = true
           env.closeTag = "</strong>"
         } else {
           tr.push("<em>")
-          env.inEmphasis = true
           env.closeTag = "</em>"
         }
         env.inline = true
@@ -64,10 +73,10 @@ function parse (chunk, env, tr) {
       tr.push(chunk.content)
       break
     case "text":
-      /*if (!env.closeTag) {
+      if (!env.closeTag) {
         tr.push("<p>")
         env.closeTag = "</p>"
-      }*/
+      }
       tr.push(chunk.content)
       break
     case "new line":
@@ -97,6 +106,8 @@ module.exports = function (opts) {
   var parser = through.obj(function (chunk, enc, cb) {
     env = parse(chunk, env, this)
     cb()
+  }, function () {
+    parse({type: "new line"}, env, this)
   })
 
   /*var push = parser.push
