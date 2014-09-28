fc3b64aebe5d00ee2b80afa99f0ee33350b0d46a
Alan Shaw <alan@freestyle-developments.co.uk>
2014-09-28 15:36:44 +0100

    Another potential implementation

--- index.js
+++ index.js
@@ -1,136 +1,201 @@
 var mdtok = require("md-tokenizer")
 var through = require("through2")
 var duplexer = require("duplexer2")
-var Block = require("./block")
 
-var emptyToken = {type: "whitespace", content: ""}
-
-function inList (bl) {
-  return bl.prev.tag.name == "li"
+function Block (prev, tagName) {
+  this.prev = prev // The previous block
+  this.indent = ""
+  this.tagName = tagName
+  this.contents = [] // String, function
+  this.transparent = false
+  this.inEmphasis = false
+  this.inStrongEmphasis = false
 }
 
-function inEmphasis (bl) {
-  return bl.tag.name == "em" || bl.tag.name == "strong"
+Block.prototype.open = function (tagName) {
+  this.contents.push(function () {
+    return "<" + this.tagName + ">"
+  })
+  this.tagName = tagName
+  return this
 }
 
-function parse (token, bl, tr) {
+function BlockEnd (start) {
+  this.start = start
+}
+
+function indentAmount (whitespace) {
+  return whitespace[0] == " " ? Math.floor(whitespace.length / 4) : whitespace.length
+}
+
+function closeEnds (ends, bl, tr) {
+  //console.log(ends.map(function (e) {
+  //  return e.start.tagName
+  //}).reverse())
+  if (!bl.prev) return ends
+
+  ends = ends.slice()
+
+  var prev = bl.prev
+  var prevs = []
+
+  while (prev) {
+    prevs.unshift(prev)
+    prev = prev.prev
+  }
+
+  prevs.forEach(function (p) {
+    // Push out all the previous block's content
+    for (var i = 0; i < p.contents.length; i++) {
+      var content = p.contents[i]
+      if (typeof content == "function") {
+        tr.push(content.call(p))
+      } else {
+        tr.push(content)
+      }
+    }
+  })
+
+  // Difference between indent determines how far to chomp through ends
+  var prevIndentAmount = indentAmount(bl.prev.indent)
+  var currentIndentAmount = indentAmount(bl.indent)
+
+  //console.log("prevIndentAmount", prevIndentAmount)
+  //console.log("currentIndentAmount", currentIndentAmount)
+
+  // Close everything?
+  if (prevIndentAmount > currentIndentAmount && currentIndentAmount == 0) {
+    var end = ends.pop()
+    while (end) {
+      //console.log("</" + end.start.tagName + ">")
+      tr.push("</" + end.start.tagName + ">")
+      end = ends.pop()
+    }
+  // Close subset?
+  } else if (prevIndentAmount >= currentIndentAmount) {
+    for (var j = 0; j < (prevIndentAmount - currentIndentAmount) + 1; j++) {
+      var end = ends.pop()
+      //console.log("</" + end.start.tagName + ">")
+      tr.push("</" + end.start.tagName + ">")
+      // If transparent then don't consume an iteration
+      if (end.start.transparent && ends.length) j--
+    }
+  }
+
+  // We no longer need this previous block - send for gc
+  bl.prev = null
+
+  return ends
+}
+
+function parse (token, bl, ends, tr) {
+  var oldEnds = ends
+
   switch (token.type) {
     case "heading":
-      if (!bl.tag.name) {
-        bl.closePrev()
+      if (!bl.tagName) {
         bl.open("h" + token.content.length)
+        ends = closeEnds(ends, bl, tr)
+        ends.push(new BlockEnd(bl))
       } else {
-        tr.push(token.content)
+        bl.contents.push(token.content)
       }
       break
+    case "underline equal":
+      bl.prev.tagName = "h1"
+      break
+    case "underline dash":
+      bl.prev.tagName = "h2"
+      break
     case "star":
-      if (!bl.tag.name) {
+      if (!bl.tagName) {
+        ends = closeEnds(ends, bl, tr)
 
-        if (!inList(bl)) {
-          bl.closeAllPrev()
-          bl.open("ul")
-          bl = new Block(bl, tr, "li")
-        } else {
-          // We are already in a list
-          var ul = bl
-
-          while (ul.tag.name != "ul") {
-            ul = ul.prev
-          }
-
-          var currentIndent = ul.tokens[0] || emptyToken
-
-          if (currentIndent.type != "whitespace") {
-            currentIndent = emptyToken
-          }
-
-          var newIndent = bl.tokens[0] || emptyToken
-
-          if (newIndent.type != "whitespace") {
-            newIndent = emptyToken
-          }
-
-          if (currentIndent.content.length == newIndent.content.length) {
-            // Same level, same indent
-            bl.closePrev()
-            bl.open("li")
-          } else if (currentIndent.content.length < newIndent.content.length) {
-            // Deeper level
+        // Something was closed
+        if (oldEnds.length != ends.length) {
+          // Was the last thing closed an <li>?
+          if (oldEnds.slice(ends.length)[0].start.tagName == "li") {
+            // Already in a list
+          } else {
+            // Need to start a new list
             bl.open("ul")
-            bl = new Block(bl, tr, "li")
-          } else if (currentIndent.content.length > newIndent.content.length) {
-            // Shallower level
-            // TODO: determine correct level
-            bl.closePrev()
-            ul.close()
-            ul.prev.close()
-            bl = new Block(bl, tr, "li")
+            bl.transparent = true
+            ends.push(new BlockEnd(bl))
+            bl = new Block(bl)
+            bl.indent = bl.prev.indent // Maintain indent
           }
-        }
-      } else {
-        if (!inEmphasis(bl)) {
-          bl = new Block(bl, tr, "em", true)
         } else {
-          tr.push("</" + bl.tag.name + ">")
-          bl = bl.prev
+          bl.open("ul")
+          bl.transparent = true
+          ends.push(new BlockEnd(bl))
+          bl = new Block(bl)
+          bl.indent = bl.prev.indent // Maintain indent
         }
+
+        bl.open("li")
+        ends.push(new BlockEnd(bl))
+      } else {
+        bl.contents.push("<" + (bl.inEmphasis ? "/" : "") + "em>")
+        bl.inEmphasis = !bl.inEmphasis
       }
       break
     case "emphasis":
-      if (!bl.tag.name) {
-        bl.closeAllPrev().open("p")
+      if (!bl.tagName) {
+        bl.open("p")
+        ends = closeEnds(ends, bl, tr)
+        ends.push(new BlockEnd(bl))
       }
 
-      if (!inEmphasis(bl)) {
-        bl = new Block(bl, tr)
-        if (token.content == "__" || token.content == "**") {
-          bl = new Block(bl, tr, "strong", true)
-        } else {
-          bl = new Block(bl, tr, "em", true)
-        }
+      if (token.content == "__" || token.content == "**") {
+        bl.contents.push("<" + (bl.inStrongEmphasis ? "/" : "") + "strong>")
+        bl.inStrongEmphasis = !bl.inStrongEmphasis
       } else {
-        tr.push("</" + bl.tag.name + ">")
-        bl = bl.prev
+        bl.contents.push("<" + (bl.inEmphasis ? "/" : "") + "em>")
+        bl.inEmphasis = !bl.inEmphasis
       }
       break
     case "whitespace":
-      bl.pushOrQueueContent(token.content)
+      if (!bl.tagName) {
+        bl.indent = token.content
+      }
+      bl.contents.push(token.content)
       break
     case "text":
-      if (!bl.tag.name) {
-        bl.closeAllPrev().open("p")
+      if (!bl.tagName) {
+        bl.open("p")
+        ends = closeEnds(ends, bl, tr)
+        ends.push(new BlockEnd(bl))
       }
-      tr.push(token.content)
+      bl.contents.push(token.content)
       break
     case "new line":
-      bl = new Block(bl, tr)
-      bl.pushOrQueueContent(token.content)
+      bl = new Block(bl)
+      bl.contents.push(token.content)
       break
     case "end":
-      bl.closeAllPrev().close()
+      closeEnds(ends, new Block(bl), tr)
       break
-    default: throw new Error("Failed to parse " + token.content)
+    default: throw new Error("Unknown token '" + token.type + "' with content '" + token.content + "'")
   }
 
-  if (token.type != "new line") {
-    bl.tokens.unshift(token)
-  }
-
-  return bl
+  return {block: bl, ends: ends}
 }
 
 module.exports = function (opts) {
-  var bl = null
+  var bl = new Block()
+  var ends = []
 
   var parser = through.obj(function (token, enc, cb) {
-    bl = parse(token, bl, this)
+    var bits = parse(token, bl, ends, this)
+    bl = bits.block
+    ends = bits.ends
     cb()
   }, function () {
-    parse({type: "end"}, bl, this)
+    parse({type: "end"}, bl, ends, this)
+    bl = new Block()
+    ends = []
     this.push(null)
   })
-
-  bl = new Block(null, parser)
 
   var tokenizer = mdtok()
   tokenizer.pipe(parser)
--- test/test.js
+++ test/test.js
@@ -5,21 +5,56 @@ var logger = require("./util/logger")
 
 test("parse", function (t) {
   var md = multiline.stripIndent(function () {/*
-    # Heading 1
+    Markdown Ipsum Presents
+    =======================
+
+    **Pellentesque habitant morbi tristique** senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. _Aenean ultricies mi vitae est_. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, `commodo vitae`, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum  rutrum orci, sagittis tempus lacus enim ac dui. [Donec non enim](#) in turpis pulvinar facilisis. Ut felis.
+
+    Header Level 2
+    --------------
+
+      1. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+      2. Aliquam tincidunt mauris eu risus.
+
+
+    > Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur  massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
+
+    ### Header Level 3
+
+      * Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+      * Aliquam tincidunt mauris eu risus.
+
+    ```
+    #header h1 a {
+      display: block;
+      width: 300px;
+      height: 80px;
+    }
+    ```
+  */})
+
+  var md = multiline.stripIndent(function () {/*
+    Heading 1
+    ===
     Some *italic* and __bold__ in the title
     **and** also in some _text_
-    ## Heading 2
+
+    Heading 2
+    ---------  
     Some more text
+
+    ### Heading 3
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+
     * Bullet 1
         * Sub bullet 1
         * Sub bullet 2
-          * Third
-          * Third *2*
+    * Third
+    * Third *2*
         * Sub bullet 3
     * Bullet 2
     * Bullet 3
         * Another sub
-
     More regular text
   */})
 
